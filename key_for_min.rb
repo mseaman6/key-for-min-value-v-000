fefb1100783779e3f9cb96fed2550ce809fc8a7c
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-09 20:27:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_value_key = nil
   name_hash.each do |key, value|
-    if !min_value || min_value > value
+    if min_value || min_value > value
       min_value = value
       min_value_key = key
     end
